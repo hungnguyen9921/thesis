0d0b25d76e64e40d734377158fd7d46aeaa5746e
hungnguyen9921 <hungvtvt01@gmail.com>
2023-03-17 22:54:04 +0700

    update websocket

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import api from './routes/api.js';
 import globalErrorHandler from './controllers/errorController.js';
 import AppError from './utils/appError.js';
@@ -11,15 +12,19 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(morgan('tiny'));
+
 app.use('/api', api);
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, './frontend/build')));
+if (process.env.ENVIRONMENT === 'production') {
+  app.use(express.static(path.join(__dirname, './frontend/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-});
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  });
+}
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- backend/webpack.config.cjs
+++ backend/webpack.config.cjs
@@ -8,30 +8,33 @@ console.log('environment:::::', environment);
 let ENVIRONMENT_VARIABLES = {
   'process.env.ENVIRONMENT': JSON.stringify('development'),
   'process.env.PORT': JSON.stringify('3080'),
-  'process.env.MONGO_CONNECTION_STRING': JSON.stringify(
-    'mongodb://thesis:lplsxlAykVxLYM3PUX46fowYontnyM48zUsPmQlmwBHKLlADmg3E7shh4mRn48dxb36jaiZRc61ZACDbUBONxA==@thesis.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@thesis@'
+  'process.env.DATABASE': JSON.stringify(
+    'mongodb+srv://theeleven:<password>@face.cly0nzp.mongodb.net/?retryWrites=true&w=majority'
   ),
+  'process.env.DATABASE_PASSWORD': JSON.stringify('GgLYZ3uEv70JkuvG'),
   'process.env.JWT_SECRET': JSON.stringify('HUNGJWT123'),
   'process.env.JWT_EXPIRES_IN': JSON.stringify('90d'),
 };
 
 if (environment === 'test') {
   ENVIRONMENT_VARIABLES = {
-    'process.env.ENVIRONMENT': JSON.stringify('test'),
+    'process.env.ENVIRONMENT': JSON.stringify('development'),
     'process.env.PORT': JSON.stringify('3080'),
-    'process.env.MONGO_CONNECTION_STRING': JSON.stringify(
-      'mongodb://thesis:lplsxlAykVxLYM3PUX46fowYontnyM48zUsPmQlmwBHKLlADmg3E7shh4mRn48dxb36jaiZRc61ZACDbUBONxA==@thesis.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@thesis@'
+    'process.env.DATABASE': JSON.stringify(
+      'mongodb+srv://theeleven:<password>@face.cly0nzp.mongodb.net/?retryWrites=true&w=majority'
     ),
+    'process.env.DATABASE_PASSWORD': JSON.stringify('GgLYZ3uEv70JkuvG'),
     'process.env.JWT_SECRET': JSON.stringify('HUNGJWT123'),
     'process.env.JWT_EXPIRES_IN': JSON.stringify('90d'),
   };
 } else if (environment === 'production') {
   ENVIRONMENT_VARIABLES = {
-    'process.env.ENVIRONMENT': JSON.stringify('production'),
+    'process.env.ENVIRONMENT': JSON.stringify('development'),
     'process.env.PORT': JSON.stringify('80'),
-    'process.env.MONGO_CONNECTION_STRING': JSON.stringify(
-      'mongodb://thesis:lplsxlAykVxLYM3PUX46fowYontnyM48zUsPmQlmwBHKLlADmg3E7shh4mRn48dxb36jaiZRc61ZACDbUBONxA==@thesis.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@thesis@'
+    'process.env.DATABASE': JSON.stringify(
+      'mongodb+srv://theeleven:<password>@face.cly0nzp.mongodb.net/?retryWrites=true&w=majority'
     ),
+    'process.env.DATABASE_PASSWORD': JSON.stringify('GgLYZ3uEv70JkuvG'),
     'process.env.JWT_SECRET': JSON.stringify('HUNGJWT123'),
     'process.env.JWT_EXPIRES_IN': JSON.stringify('90d'),
   };
